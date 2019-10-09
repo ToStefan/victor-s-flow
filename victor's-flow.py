cdf71a1a5321f40a5310c70a1f096292f92c71e0
Stefan <stefam1996@live.com>
2019-10-09 02:15:23 +0200

    flow optimization

--- victor's-flow.py
+++ victor's-flow.py
@@ -5,6 +5,12 @@ from utils import *
 
 def main():
 
+	front_end()
+
+	back_end()
+
+def front_end():
+
 	production_update_config_js()
 
 	commit_push_front_end(sys.argv[1])
@@ -15,11 +21,13 @@ def main():
 
 	development_update_config_js()
 
+def back_end():
+
 	production_update_application_yml()
 
 	commit_push_back_end(sys.argv[1])
 
-	development_update_application_yml()
+	development_update_application_yml()	
 
 if __name__ == '__main__':
 	main()
